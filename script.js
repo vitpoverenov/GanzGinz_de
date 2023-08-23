ad65877e0cfa5cadd6ea6f2b86cd5bf643e99d54
Vitalii Povierienov | Front-end developer <73063388+vitpoverenov@users.noreply.github.com>
2023-08-23 21:43:57 +0300

    Add new reveal animated to nav. Delay 800.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let menu = document.querySelector ('#menu-icon') ;
-let navlist = document.querySelector ('#navlist') ;
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-    menu.classList.toggle('')
+    navlist.classList.toggle('active'); // Toggle 'active' class
 }
 
-const rv = ScrollReveal ({
+const sr = ScrollReveal({
     distance: '40px',
     duration: 2500,
     reset: true
 });
 
-sr.reveal ('.container h6',{ delay: 150, origin: 'left'});
-
-sr.reveal ('.container h3',{ delay: 250, origin: 'right'});
-
-sr.reveal ('.container h1',{ delay: 400, origin: 'top'});
+sr.reveal('.container h6', { delay: 150, origin: 'left' });
+sr.reveal('.container h3', { delay: 250, origin: 'right' });
+sr.reveal('.container h1', { delay: 400, origin: 'top' });
+sr.reveal('.container p', { delay: 600, origin: 'right' });
+sr.reveal('.social', { delay: 600, origin: 'top' })
+sr.reveal('.logo', { delay: 600, origin: 'right' })
+sr.reveal('.navlist', { delay: 800, origin: 'right' });
