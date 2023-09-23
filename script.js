7ce8f6c82f9e557a1e40c033ec219704ba23e360
Vitalii Povierienov | Front-end developer <73063388+vitpoverenov@users.noreply.github.com>
2023-09-23 15:22:07 +0300

    Add new animating effect on btn download.

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ sr.reveal('.container h6', { delay: 150, origin: 'left' });
 sr.reveal('.container h3', { delay: 250, origin: 'right' });
 sr.reveal('.container h1', { delay: 400, origin: 'top' });
 sr.reveal('.container p', { delay: 600, origin: 'right' });
-sr.reveal('.social', { delay: 600, origin: 'top' })
-sr.reveal('.logo', { delay: 600, origin: 'right' })
+sr.reveal('.social', { delay: 600, origin: 'top' });
+sr.reveal('.logo', { delay: 600, origin: 'right' });
 sr.reveal('.navlist', { delay: 800, origin: 'right' });
+sr.reveal('.main-btn', { delay: 1200, origin: 'top right' });
+sr.reveal('.right-nav', { delay: 200, origin: 'top right' });
